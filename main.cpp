65dd9b1c012ee41f7fd12148f07b4cca0c89de70
Gabriel Arreco <>
2021-05-23 00:52:29 +0000

    Realizado acerto na linha 15 e compilado para testar a interação com usuário

--- main.cpp
+++ main.cpp
@@ -1,5 +1,25 @@
 #include <iostream>
+using namespace std;
+#include <cstring>
+
+int funcao (char*, char, char, char*);
 
 int main() {
-  std::cout << "Hello World!\n";
+  int num=0;
+  char palavra[70];
+  char palavratrocada;
+  char palaatual;
+  char palatrocada;
+
+ cout << "Digite a palavra a ser codificada" << endl;
+ cin.getline(palavra, 70);
+
+ cout << "Digite uma letra que será substituida" << endl;cin >> palaatual;
+ fflush (stdin);
+
+ cout << "Digite a letra para substituir a outra" << endl;cin >> palatrocada;
+ fflush (stdin);
+
+ 
+
 }
